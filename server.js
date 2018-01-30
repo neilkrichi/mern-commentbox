3b1a30360765b744f4bfb1436dcba2d76b764f4f
neilkrichi <neilkrichi@gmail.com>
2018-01-30 14:58:37 -0800

    added comments model

--- server.js
+++ server.js
@@ -4,12 +4,17 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var Comment = require('./model/comments');
+
 //and create our instances
 var app = express();
 var router = express.Router();
 //set our port to either a predetermined port number if you have set
 //it up, or 3001
 var port = process.env.API_PORT || 3001;
+
+//db config
+mongoose.connect('mongodb://neilkrichi:neilkr92@ds119078.mlab.com:19078/commentbox');
 
 //now we should configure the API to use bodyParser and look for
 //JSON data in the request body
